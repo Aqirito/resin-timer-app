394e674848286e7f955b41215c0bce7302e3a871
Aqirito <aqil.rahmat99@gmail.com>
2022-06-19 03:59:02 +0800

    Added localNotification plugins and configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'resin-timer',
+  appId: 'com.resin.timer',
+  appName: 'Resin Timer',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav",
+    },
+  },
 };
 
 export default config;
