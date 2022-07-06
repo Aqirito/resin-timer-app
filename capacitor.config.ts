ea07cd2dc77e4fc73e586df39c0d69485f7a8cb2
Aqirito <aqil.rahmat99@gmail.com>
2022-07-07 01:47:56 +0800

    UPDATE: change notifcation sound

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,7 +9,7 @@ const config: CapacitorConfig = {
     LocalNotifications: {
       smallIcon: "ic_stat_icon_config_sample",
       iconColor: "#488AFF",
-      sound: "beep.wav",
+      sound: "zhongli.wav",
     },
   },
 };
